c26f9b54a5985fab351f1c6875b8cff65782be86
Rafael Lucas <rafaellucasdacosta42@gmail.com>
2018-07-04 19:55:37 -0300

    Cadastro

--- Cadastro.js
+++ Cadastro.js
@@ -1,23 +1,23 @@
 function contaCharNome(){
-  var totalchar = 0 + document.getElementById("nomeCompleto").value.length;
+  var totalchar = -1 + document.getElementById("nomeCompleto").value.length;
   if (totalchar < 50){
     totalchar++;
   }
-  document.getElementById("qntCharNome").innerHTML= totalchar + "/50";
+  document.getElementById("qntCharNome").innerHTML= totalchar + " /50";
 }
 
 function contaCharUsername(){
-  var totalchar = 0 + document.getElementById("user").value.length;
+  var totalchar = -1 + document.getElementById("user").value.length;
   if (totalchar < 25){
     totalchar++;
   }
-  document.getElementById("qntCharUsername").innerHTML= totalchar + "/25";
+  document.getElementById("qntCharUsername").innerHTML= totalchar + " /25";
 }
 
 function contaCharEmail(){
-  var totalchar = 0 + document.getElementById("email").value.length;
+  var totalchar = -1 + document.getElementById("email").value.length;
   if (totalchar < 40){
     totalchar++;
   }
-  document.getElementById("qntCharEmail").innerHTML= totalchar + "/60";
+  document.getElementById("qntCharEmail").innerHTML= totalchar + " /60";
 }
