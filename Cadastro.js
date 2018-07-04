e67e96ae07c5f5d28df32a431c648d78c3ef9c99
Rafael Lucas <rafaellucasdacosta42@gmail.com>
2018-07-04 19:48:05 -0300

    Cadastro

--- Cadastro.js
+++ Cadastro.js
@@ -1,23 +1,23 @@
 function contaCharNome(){
-  var totalchar = 50 - document.getElementById("nomeCompleto").value.length;
-  if (totalchar < 0){
-    totalchar--;
+  var totalchar = 0 + document.getElementById("nomeCompleto").value.length;
+  if (totalchar < 50){
+    totalchar++;
   }
-  document.getElementById("qntCharNome").innerHTML="Caracteres restantes:"+ totalchar + "/50";
+  document.getElementById("qntCharNome").innerHTML= totalchar + "/50";
 }
 
 function contaCharUsername(){
-  var totalchar = 25 - document.getElementById("user").value.length;
-  if (totalchar < 0){
-    totalchar--;
+  var totalchar = 0 + document.getElementById("user").value.length;
+  if (totalchar < 25){
+    totalchar++;
   }
-  document.getElementById("qntCharUsername").innerHTML="Caracteres restantes:"+ totalchar + "/25";
+  document.getElementById("qntCharUsername").innerHTML= totalchar + "/25";
 }
 
 function contaCharEmail(){
-  var totalchar = 25 - document.getElementById("email").value.length;
-  if (totalchar < 0){
-    totalchar--;
+  var totalchar = 0 + document.getElementById("email").value.length;
+  if (totalchar < 40){
+    totalchar++;
   }
-  document.getElementById("qntCharEmail").innerHTML="Caracteres restantes:"+ totalchar + "/25";
+  document.getElementById("qntCharEmail").innerHTML= totalchar + "/60";
 }
