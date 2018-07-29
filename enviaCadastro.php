9cb6a8db8044e5115ead344e24394190e8794038
Chayene Goncharenco <37180815+chayeneg@users.noreply.github.com>
2018-07-29 18:04:41 -0300

    enviaCadastro.php

--- enviaCadastro.php
+++ enviaCadastro.php
@@ -25,7 +25,20 @@ else{
   }
 }
 $conn->close()
-?><br><br>
+?>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Cadastro</title>
+  <link rel="stylesheet" type="text/css" href="cadastro.css"/>
+</head>
+
+<body>
+<div class="caixa">
+  <div class="conteudo">
 <p>Cadastro efetuado com sucesso! Faça o login!! </p><input type="submit" value="Logar-se" onclick="location.href='login.php'"></a>
+</div>
+</div>
+
 </body>
 </html>
