9b722876528c03aec43b2c56b2d860d6b3bad530
Rafael  Lucas <rafaellucasdacosta42@gmail.com>
2018-07-31 12:38:17 -0300

    Validação

--- enviaCadastro.php
+++ enviaCadastro.php
@@ -6,39 +6,33 @@ $nome = $_POST['nome'];
 $usuario = $_POST['usuario'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+$confirmaSenha = $_POST['confirmasenha'];
 $dtNasc = $_POST['data'];
 $sexo = $_POST['sexo'];
 $pais = $_POST['pais'];
 $site = $_POST['site'];
+$query_select = "SELECT usuario FROM users WHERE usuario = '$usuario'";
+$select = mysqli_query($conn, $query_select);
+$resultado = $select->fetch_assoc();
 
 if($nome==""||$usuario==""||$email==""||$senha==""||$dtNasc==""||$sexo==""||$pais==""){
-  echo "Os campos marcados com '*' devem ser preenchidos!";
+  echo "<script language'javascript' type='text/javascript'>alert('Os campos com * devem ser preenchidos');window.location.href='cadastro.php';</script>";
+}
+else if($senha!=$confirmaSenha){
+  echo "<script language'javascript' type='text/javascript'>alert('As senhas não conferem!');window.location.href='cadastro.php';</script>";
+}
+else if($usuario==$resultado["usuario"]){
+  echo "<script language'javascript' type='text/javascript'>alert('O usuário já está cadastrado!');window.location.href='cadastro.php';</script>";
 }
 else{
   $sql = "INSERT INTO users (nome, usuario, email, senha, dataNasc, sexo, pais, website)
   VALUES ('$nome','$usuario','$email','$senha','$dtNasc','$sexo','$pais','$site')";
   $execSQL = mysqli_query($conn, $sql);
 
-
-  if ($conn->query($sql) === TRUE) {
-    echo "Cadastro efetuado com sucesso!";
+  if ($execSQL) {
+    echo "<script language'javascript' type='text/javascript'>alert('Cadastro efetuado!');window.location.href='login.php';</script>";
   }
 }
+
 $conn->close()
 ?>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Cadastro</title>
-  <link rel="stylesheet" type="text/css" href="cadastro.css"/>
-</head>
-
-<body>
-<div class="caixa">
-  <div class="conteudo">
-<p>Cadastro efetuado com sucesso! Faça o login!! </p><input type="submit" value="Logar-se" onclick="location.href='login.php'"></a>
-</div>
-</div>
-
-</body>
-</html>
