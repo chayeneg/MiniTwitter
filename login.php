603935e80a473ab1f2dcf494a95bd09660ae13d5
Chayene Goncharenco <cgoncharenco@gmail.com>
2018-08-19 14:04:54 -0300

    Login e Processa Login

---
+++ login.php
@@ -0,0 +1,15 @@
+<html lang="pt-br">
+  <head>
+    <title>Mini-Twitter</title>
+  </head>
+  <body>
+    <h1>Mini-Twitter</h1>
+    <h2>Login</h2>
+    <p>Utilize seu nome de usuário e senha para se conectar.</p>
+    <form action="processa-login.php" method="post">
+      <input type="text" id="username" name="username" size="40" placeholder="Nome de usuário" required><br><br>
+      <input type="password" id="password" name="password" size="40" placeholder="Mínimo de 8 caracteres" required><br><br>
+      <input type="submit" id="submit" size="40" value="Logar"><br><br>
+    </form>
+  </body>
+</html>
---
+++ processa-login.php
@@ -0,0 +1,23 @@
+<?php
+  require_once('conexao.php');
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $query = "select username, id, password from users where username = '$username' ";
+  if ($result = $conn->query($query)) {
+    if ($row = $result->fetch_object()){
+      if (strcmp($row->password, $password)==0){
+        session_start();
+        $_SESSION['username'] = $row->username;
+        $_SESSION['id'] = $row->id;
+        header("Location: timeline.php");
+      } else {
+        echo "<script language'javascript' type='text/javascript'>alert('Login ou senha incorreto(s).');window.location.href='login.php';</script>";
+      }
+    } else {
+      echo "<script language'javascript' type='text/javascript'>alert('Erro desconhecido!');window.location.href='cadastro.php';</script>";
+    }
+  }
+  else {
+    echo "<script language'javascript' type='text/javascript'>alert('Erro no banco de dados!');window.location.href='login.php';</script>";
+  }
+?>
