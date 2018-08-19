6d0afc29d1a8fb14d4de7f8f312b4bb57fdd426f
Chayene Goncharenco <cgoncharenco@gmail.com>
2018-08-19 15:31:57 -0300

    Responsividade

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <html lang="pt-br">
   <head>
     <title>Mini-Twitter</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
+    <div class="main">
+      <div class="content">
+        <div class="content-inner">
+
     <h1>Mini-Twitter</h1>
     <p>Venha contar suas frustrações</p>
     <a id="link" href="cadastro.php">Inscrever-se</a>
     <br>
     <a id="link" href="login.php">Conectar-se</a>
+
+        </div>
+      </div>
+
+      <div class="sidebar">
+        <p> teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  teste  teste  teste teste  teste  teste  teste  teste  test </p>
+
+      </div>
+  </div>
+
+
+
   </body>
 </html>
