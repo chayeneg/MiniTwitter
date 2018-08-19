f75e45a922b1986da70da84677e57fd39934c4bb
ChayeneGoncharenco <cgoncharenco@gmail.com>
2018-08-19 20:41:32 -0300

    CSS da Página de Login

--- login.php
+++ login.php
@@ -1,9 +1,28 @@
 <html lang="pt-br">
   <head>
     <title>Mini-Twitter</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
   </head>
   <body>
-    <h1>Mini-Twitter</h1>
+    <div class="main">
+      <div class="titulo">
+          <h1>Mini Twitter</h1>
+      </div>
+      <div id="menu">
+				<ul>
+					<li> <a href="index.php"> PÁGINA INICIAL </a> </li>
+          <li> <a href="cadastro.php"> CADASTRAR </a> </li>
+          <li> <a href="login.php"> LOGAR </a> </li>
+				</ul>
+			</div>
+
+      <div class="content"><br><br>
+              <fieldset>
+
+        <div class="content-inner">
+
     <h2>Login</h2>
     <p>Utilize seu nome de usuário e senha para se conectar.</p>
     <form action="processa-login.php" method="post">
@@ -11,5 +30,15 @@
       <input type="password" id="password" name="password" size="40" placeholder="Mínimo de 8 caracteres" required><br><br>
       <input type="submit" id="submit" size="40" value="Logar"><br><br>
     </form>
+  </div>
+</div>
+
+<br style="clear:both">
+    </fieldset>
+<div class="footer">
+  Copyright©2018, Fayenel Company.
+    <br>Todos os direitos reservados.
+</div>
+</div>
   </body>
 </html>
