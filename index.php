ae49517f4188cc24d05ff338c2744d7158d96967
ChayeneGoncharenco <cgoncharenco@gmail.com>
2018-08-19 19:56:16 -0300

    Responsividade e CSS

--- index.php
+++ index.php
@@ -2,30 +2,40 @@
   <head>
     <title>Mini-Twitter</title>
       <link rel="stylesheet" type="text/css" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
   </head>
   <body>
     <div class="main">
-      <div class="menu">
+      <div class="titulo">
+          <h1>Mini Twitter</h1>
+      </div>
+      <div id="menu">
 				<ul>
-					<li> <a href="index.php"> Home </a> </li>
-					<li> <a href="login.php"> Entrar </a> </li>
+					<li> <a href="index.php"> Página Inicial </a> </li>
           <li> <a href="cadastro.php"> Cadastrar </a> </li>
-
+          <li> <a href="login.php"> Logar </a> </li>
 				</ul>
 			</div>
-      <div class="content">
+
+      <div class="content"><br><br>
+              <fieldset>
+
         <div class="content-inner">
+           <h2>Bem Vindo ao Mini Twitter!</h2> <br>
 
-    <h1>Mini-Twitter</h1>
-    <p>Venha contar suas frustrações</p>
+              Aqui você pode encontrar amigos, conversar e ver tudo que está acontecendo de bom (ou ruim) na vida de quem você gosta de stalkear!<br><br>
 
+
+              Use com Moderação!
         </div>
       </div>
-      <br style="clear:both">
 
-      <br><br>
+      <br style="clear:both">
+          </fieldset>
 			<div class="footer">
-				Rodapé
+				Copyright©2018, Fayenel Company.
+          <br>Todos os direitos reservados.
 			</div>
   </div>
 
