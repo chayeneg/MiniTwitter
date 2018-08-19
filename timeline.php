68aa96c9832b4c7dacd9b7596ac3d0967fb1d140
Rafael Lucas <rafaellucasdacosta42@gmail.com>
2018-08-19 20:04:13 -0300

    timeline

--- timeline.php
+++ timeline.php
@@ -14,19 +14,17 @@ $name = "$auth_name";
   <h1>Mini-Twitter</h1>
   <h2>Timeline</h2>
   <form action="processa-timeline.php" method="post">
-    <fieldset id="login-box">
       <input type="button" id="profile-btn" value="Perfil" onclick="location.href='perfil.php'">
       <input type="button" id="profile-btn" value="Sair" onclick="location.href='logout.php'"><br>
       <?php
       echo $name . "<br>" . "@" . $username . "<br>";
       ?>
-    </fieldset><br>
+      <br>
     <textarea class="post" id="new_message" name="new_message" placeholder="O que você está pensando?" rows="5" cols="40" onkeyup="conta(140)" maxlength="140"></textarea>
     <p id="char_count" name="char_count"> 140 caracteres restantes </p>
     <input type="submit" id="btn-submit" name="btn-submit" value="Twettar"><br><br>
   </form>
     <h2> Veja o que estão falando </h2>
-    <fieldset>
       <?php
         $sql = "SELECT users.name, users.username, tweets.date, tweets.message FROM users INNER JOIN tweets ON users.id = tweets.user_id ORDER BY tweets.date DESC";
         $result = mysqli_query($conn, $sql);
@@ -43,7 +41,6 @@ $name = "$auth_name";
           echo "aida não existem mensagens postadas.";
         }
       ?>
-    </fieldset>
 
 </body>
 </html>
