a5582c46343227d787403a4ddac93d68cab608c2
Rafael Lucas <rafaellucasdacosta42@gmail.com>
2018-08-24 00:18:11 -0300

    deleta

--- deleta.php
+++ deleta.php
@@ -3,8 +3,9 @@
 
   require_once('conexao.php');
   $id_tweet = $_SESSION['tweets_id'];
+  $id_user = $_SESSION['id'];
 
-  $sql = "DELETE FROM tweets WHERE id = $id_tweet";
+  $sql = "DELETE FROM tweets WHERE id = $id_tweet and user_id = $id_user";
   if ($conn->query($sql) === TRUE) {
     header("location:timeline.php");
   }
--- verifica-usuario.php
+++ verifica-usuario.php
@@ -2,6 +2,7 @@
   session_start();
   $auth_username = $_SESSION['username'];
   $auth_name = $_SESSION['name'];
+  $auth_id = $_SESSION['id'];
   if (!isset($auth_username)){
      header("Location: login.php");
   }
